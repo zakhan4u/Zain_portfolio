1486856e05dc1b17dc22da662753bad8cb90bcaf
YourGitHubUsername <YourGitHubEmail>
2025-02-26 12:26:24 +0500

    new changes on 26

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,22 @@
-import ClientWrapper from "@/components/ClientWrapper";
+
 import Recentprojects from "@/components/Recentprojects";
 import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 import { FloatingNavDemo } from "@/components/Navigation";
+import Hero from "@/components/Hero";
+import Grid from "@/components/Grid";
 
 
 export default function Home() {
   return (
-    <main className="relative bg-black justify-center px-5 items-center overflow-hidden mx-auto sm:px-10 flex-col">
+    <main className="relative bg-black-100 flex min-h-screen justify-center items-center overflow-hidden mx-auto sm:px-10 flex-col">
       <div className="max-w-7xl w-full">
         <h1 className="text-3xl justify-center items-center">
           This is the Zain portfolio website
         </h1>
         <FloatingNavDemo/>
-        <ClientWrapper /> {/* Now safely wrapping client components */}
+       <Hero/>
+       <Grid/>
         <Recentprojects/>
         <Experience/>
         <Footer/>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ import { FaLocationArrow } from 'react-icons/fa'
 
 const Hero = () => {
   return (
-    <div className='pb-20 pt-36'>
+    <div>
         <div>
             <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white'/>
             <Spotlight className='-top-30 -left-20' fill='white'/>
